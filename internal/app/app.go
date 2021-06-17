2459f073411636da42ef38d85145fb023e1d8e2c
Max <xmavrik74@gmail.com>
2021-06-17 14:25:20 +0500

    Minor fixes

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,43 +1,44 @@
 package app
 
 import (
+	"context"
 	"errors"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 	"wiltonassessment/internal/cond"
 	"wiltonassessment/internal/config"
 )
 
 func Run(path string) error {
-	errs := make(chan error)
-
 	cfg, err := config.GetConfig(path)
 	if err != nil {
-		return err
+		log.Fatalf("Error occured during reading configuration file: %v\n", err)
 	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/api/data", cond.GetInputData)
 	srv := NewServer(cfg, mux)
 
-	terminating := make(chan os.Signal, 1)
-	signal.Notify(terminating, syscall.SIGTERM, syscall.SIGINT)
-	<-terminating
-
 	go func() {
+		log.Println("Server started", srv.httpServer.Addr)
 		if err := srv.Run(); !errors.Is(err, http.ErrServerClosed) {
-			errs <- err
+			log.Fatalf("Error occured during starting the server: %v\n", err)
 		}
 	}()
+	<-stop
 
-	for err := range errs {
-		if err != nil {
-			return err
-		}
-		return nil
+	ctx, shutdown := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdown()
+
+	if err := srv.Stop(ctx); err != nil {
+		log.Fatalf("Error occured during stopping the server: %v\n", err)
 	}
-
 	return nil
 }
